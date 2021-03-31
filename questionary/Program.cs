59fc1aed79a8f719265efe70004701f68bd1ca0c
Alexey Sorokin <asorokin@ukaachen.de>
2021-03-31 19:51:27 +0200

    clean up

--- questionary/Program.cs
+++ questionary/Program.cs
@@ -16,6 +16,39 @@ namespace questionary {
     class Program {
         static void Main(string[] args) {
 
+            (string name, int age) anketa;
+            anketa.name = Console.ReadLine();
+
+            Console.WriteLine("Input your fav color");
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+
             // как вас зовут, возраст, дату рождения.
             Console.WriteLine("What is your name?");
             var MyName = Console.ReadLine();
@@ -26,6 +59,7 @@ namespace questionary {
 
             Console.WriteLine("Hello {0}! You are still quite young with your {1}! You birthday is nice {2}!",
                 MyName, MyAge, MyBirthday);
+
             Console.ReadKey();
         
         }
