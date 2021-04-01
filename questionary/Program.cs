9c9ca5102d4fb60a3382df8b024af29cb691ceaa
Alexey Sorokin <asorokin@ukaachen.de>
2021-04-01 14:57:40 +0200

    questinary with checking

--- questionary/Program.cs
+++ questionary/Program.cs
@@ -2,66 +2,113 @@
 
 namespace questionary {
 
-    enum DaysOfWeek : byte
-    {
-        Monday, 
-        Tuesday, 
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
-    }
 
     class Program {
-        static void Main(string[] args) {
 
-            (string name, int age) anketa;
+
+        static int GetCorrectNumber(string greetings)
+        {
+            string number;
+            int intnumber;
+            do
+            {
+                Console.WriteLine(greetings);
+                number = Console.ReadLine();
+            } while (!CheckNum(number, out intnumber));
+            return intnumber;
+        }
+        static bool CheckNum(string number, out int corrnumber)
+        {
+            if(int.TryParse(number, out int intnum))
+            {
+                if(intnum > 0)
+                {
+                    corrnumber = intnum;
+                    return true;
+                }
+            }
+            {
+                corrnumber = 0;
+                return false;
+            }
+            
+        }
+        static string[] CreateArr(in int num)
+        {
+            string[] arr = new string[num];
+            return arr;
+        }
+        static void PrintUserInfo((string name, string surname, int age, bool hasPet, int amountPets, string[] pets, int amountFavColors, string[] colors) anketa)
+        {
+            Console.WriteLine("Hello, {0} {1}! You quite young with your {2}!", anketa.name, anketa.surname, anketa.age);
+            if (anketa.hasPet && anketa.amountPets > 1)
+            {
+                Console.WriteLine("Cool! You have pets!");
+                foreach (var pet in anketa.pets)
+                {
+                    Console.WriteLine("You have a nice pet {0}!", pet);
+                }
+            }
+            else if (anketa.hasPet && anketa.amountPets == 1)
+            {
+                Console.WriteLine("Cool! You have pet!");
+            }
+            foreach (var color in anketa.colors)
+            {
+                Console.WriteLine("You have a nice favorite color {0}!", color);
+            }
+            
+        }
+
+        static (string name, string surname, int age, bool hasPet, int amountPets, string[] pets, int amountFavColors, string[] colors) FillUserInfo()
+        {
+            (string name, string surname, int age, bool hasPet, int amountPets, string[] pets, int amountFavColors, string[] colors) anketa;
+
+            Console.WriteLine("type your name");
             anketa.name = Console.ReadLine();
 
-            Console.WriteLine("Input your fav color");
-            switch (Console.ReadLine())
+            Console.WriteLine("type your surname");
+            anketa.surname = Console.ReadLine();
+
+            anketa.age = GetCorrectNumber("type your age in digitals");
+
+            Console.WriteLine("do you have pets? Type yes or no");
+            string res = Console.ReadLine();
+            anketa.hasPet = false;
+            if (res == "yes") {
+                anketa.hasPet = true;
+                Console.WriteLine("how many pets do you have?");
+                anketa.amountPets = GetCorrectNumber("type amount of pets you have in digitals");
+                anketa.pets = CreateArr(anketa.amountPets);
+                for(int i = 0; i < anketa.amountPets; ++i)
+                {
+                    Console.WriteLine("type you pets' name, rest pets to type {0}", anketa.amountPets - i - 1);
+                    anketa.pets[i] = Console.ReadLine();
+                }
+            }
+            else
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
+                anketa.amountPets = 0;
+                anketa.pets = new string[anketa.amountPets];
             }
 
-            // как вас зовут, возраст, дату рождения.
-            Console.WriteLine("What is your name?");
-            var MyName = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            var MyAge = checked((byte)int.Parse(Console.ReadLine()));
-            Console.WriteLine("When was you born?");
-            var MyBirthday = Console.ReadLine();
+            Console.WriteLine("how many favorite colors do you have?");
+            anketa.amountFavColors = GetCorrectNumber("type amount of your favorite numbers"); ;
+            anketa.colors = CreateArr(anketa.amountFavColors);
+            for (int i = 0; i < anketa.amountFavColors; ++i)
+            {
+                Console.WriteLine("type you favorite color, rest colors to type {0}", anketa.amountFavColors - i - 1);
+                anketa.colors[i] = Console.ReadLine();
+            }
+            return anketa;
+        }
 
-            Console.WriteLine("Hello {0}! You are still quite young with your {1}! You birthday is nice {2}!",
-                MyName, MyAge, MyBirthday);
+        static void Main(string[] args) {
 
+            var anketa = FillUserInfo();
+            PrintUserInfo(anketa);
+            
             Console.ReadKey();
-        
         }
     }
 }
